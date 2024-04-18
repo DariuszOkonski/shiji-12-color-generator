fd2a92414e798b0b7cd94efa0727521587cd8649
Dariusz Okoński <darek200180@gmail.com>
2024-04-18 21:44:38 +0200

    add singlecolor

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,7 +1,17 @@
+import { nanoid } from 'nanoid';
 import React from 'react';
+import SingleColor from './SingleColor';
 
-const ColorList = () => {
-  return <div>ColorList</div>;
+const ColorList = ({ colors }) => {
+  // console.log(colors);
+
+  return (
+    <section className='colors'>
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid()} color={color} index={index} />;
+      })}
+    </section>
+  );
 };
 
 export default ColorList;
