282ef14c6f0ee9684e1768c1e1a6e58030d5308c
Dariusz Okoński <darek200180@gmail.com>
2024-04-19 17:56:04 +0200

    save color to clipboard

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const SingleColor = ({ color, index }) => {
   const { hex, weight } = color;
 
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      await navigator.clipboard.writeText(`#${hex}`);
+      toast.success('Color copied to clipboard');
+    } else {
+      toast.error('Failed to copy color to clipboard');
+    }
+  };
+
   return (
     <article
+      onClick={saveToClipboard}
       className={index > 10 ? 'color color-light' : 'color'}
       style={{ background: `#${hex}` }}
     >
